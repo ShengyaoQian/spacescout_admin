45e8008d6500ecf63a24f15a1facfb016455a4d5
Shengyao Qian <qians2@uw.edu>
2013-08-09 14:51:57 -0700

    Unittest for approve method in edit_space.py has been added to single space test

--- test/single_space_test.py
+++ test/single_space_test.py
@@ -11,19 +11,15 @@ from spacescout_admin.utils import *
 import json
 import oauth2
 
-class SingleSpaceTest(TestCase):
-    before_queued_space = None
-    consumer = oauth2.Consumer(key=settings.SS_WEB_OAUTH_KEY, secret=settings.SS_WEB_OAUTH_SECRET)
-    c = oauth2.Client(consumer)    
-    manager_group = None
-    user = None
 
+class SingleSpaceTest(TestCase):
+    
     def setUp(self):
         # Create a managers group
         self.managers_group = Group.objects.create(name="managers")
         # Create a user
-        self.user = User.objects.create_user(username='user', password='pass', email='user@uw.edu')
-  
+        self.user = User.objects.create_user(username='user', password='pass', email='user@example.com')
+
         # Initialize a spot that has different manager than user
         before_json = '{"manager": ["managers"], "capacity": 12, "location": {"floor": "2nd floor", "latitude": 47.652956, "room_number": 207, "longitude": -122.316343, "building_name": "Fishery Sciences (FSH)"}}'
         self.before_queued_space = {
@@ -38,83 +34,97 @@ class SingleSpaceTest(TestCase):
         }
         before_form = QueueForm(self.before_queued_space)
         self.assertEqual(before_form.is_valid(), True, "Makes sure the form is valid")
-        before_space = before_form.save(commit=False)
-        before_space.space_last_modified = "2013-05-08T20:25:09.490751+00:00"
-        before_space.modified_by = self.user
-        before_space.space_id = 1
-        before_space.save()
-        q_etag = before_space.q_etag
-   
-    def test_delete_manager(self):
+        self.before_space = before_form.save(commit=False)
+        self.before_space.space_last_modified = "2013-05-08T20:25:09.490751+00:00"
+        self.before_space.modified_by = self.user
+        self.before_space.space_id = 1
+        self.before_space.save()
+        q_etag = self.before_space.q_etag
+        consumer = oauth2.Consumer(key=settings.SS_WEB_OAUTH_KEY, secret=settings.SS_WEB_OAUTH_SECRET)
+        self.c = oauth2.Client(consumer)    
+    
+    def _delete_helper(self, can_publish, is_manager):
+        # construct the error message
+        manager_str = "a manager"
+        permission_str = "permission to delete"
+        if not is_manager:
+            manager_str = "not " + manager_str
+        if not can_publish:
+            permission_str = "no " + permission_str
+        message = "Delete failed at is " + manager_str + " and has " + permission_str 
+        # try deleting the space
+        is_deleted = edit_space._is_deleted(self.before_queued_space, is_manager, can_publish, self.user, self.before_queued_space, self.c)
+        self.assertEqual(is_deleted, is_manager and can_publish, message)
+
+    def test_delete_main(self):
+        # test: has no permission to delete and is not a manager of the space --> fail
+        can_publish = False
+        is_manager = edit_space._is_manager(self.user, self.managers_group)
+        self._delete_helper(can_publish, is_manager)
+
+        # test: has permission to delete, but is not a manager of the space --> fail
+        can_publush = True
+        self._delete_helper(can_publish, is_manager)
+
+        # test: has permission to delete and is a manager of the space ---> success
         # add the user to the managers group, so the user become a manager of the space
         self.user.groups.add(self.managers_group)
         # check if the user is a manager of the space and has the right to publish
         is_manager = edit_space._is_manager(self.user, self.managers_group)
-        can_publish = False
-        # let the user try deleting a space   
-        is_deleted = edit_space._is_deleted(self.before_queued_space, is_manager, can_publish, self.user, self.before_queued_space, self.c)
-        self.assertEqual(is_deleted, False, "Delete failed at is a manager, and cannot publish")
+        self._delete_helper(can_publish, is_manager)
 
-    def test_delete_manager_can_publish(self):
+        # test: has no permisson to delete, but is a manager of the space --> fail
+        can_publish = False
+        self._delete_helper(can_publish, is_manager)
+
+    def _approve_helper(self, is_manager, can_approve, before_status):
+        # construct the error message
+        manager_str = "a manager"
+        permission_str = "permission to approve"
+        should_be_approved = True
+        if not is_manager:
+            manager_str = "not " + manager_str
+            should_be_approved = False
+        if not can_approve:
+            permission_str = "no " + permission_str
+            should_be_approved = False
+        message = "Approve failed at is " + manager_str + " and has " + permission_str 
+       
+        after_space = self.before_space
+        # try approving the space
+        if is_manager:
+            after_space = edit_space._is_approved(self.before_queued_space, can_approve, self.before_space, self.user)
+        self.assertEqual(after_space.status ==  before_status, not should_be_approved, message)
+        # set space status back to its initial status
+        self.before_space.status = before_status
+ 
+    def test_approve_main(self):
+        self.before_queued_space["changed"] = "approved"
+        before_status = self.before_space.status
+
+        # test: has no permission to approve and is not a manager of the space --> fail
+        can_approve = False
+        is_manager = edit_space._is_manager(self.user, self.managers_group)
+        self._approve_helper(is_manager, can_approve, before_status)
+
+        #test: has permission to approve, but is a manager of the space --> fail
+        can_approve = True
+        self._approve_helper(is_manager, can_approve, before_status)
+
+        # test: has permission to approve and is a manager of the space --> success
         # add the user to the managers group, so the user become a manager of the space
         self.user.groups.add(self.managers_group)
         # check if the user is a manager of the space and has the right to publish
         is_manager = edit_space._is_manager(self.user, self.managers_group)
-        can_publish = True
-        # let the user try deleting a space
-        is_deleted = edit_space._is_deleted(self.before_queued_space, is_manager, can_publish, self.user, self.before_queued_space, self.c)
-        self.assertEqual(is_deleted, True, "Delete failed at is a manager, and can publish")
+        self._approve_helper(is_manager, can_approve, before_status)
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        # test: has no permission to approve, but is a manager of the space --> fail
+        can_approve = False
+        self._approve_helper(is_manager, can_approve, before_status)
 
-    def test_delete_can_publish(self):
-        # check if the user is a manager of the space and has the right to publish
-        is_manager = edit_space._is_manager(self.user, self.managers_group)
-        can_publish = True
-        is_deleted = edit_space._is_deleted(self.before_queued_space, is_manager, can_publish, self.user, self.before_queued_space, self.c)
-        self.assertEqual(is_deleted, False, "Delete failed at is not a manager, and can publish")
-
-
-    def test_delete_general_user(self):
-        # check if the user is a manager of the space and has the right to publish
-        is_manager = edit_space._is_manager(self.user, self.managers_group)
-        can_publish = False
-        # let the user try deleting a space
-        is_deleted = edit_space._is_deleted(self.before_queued_space, is_manager, can_publish, self.user, self.before_queued_space, self.c)
-        self.assertEqual(is_deleted, False, "Delete failed at is not a manager, and cannot publish")
-   
     def tearDown(self):
         # Delete the managers group and the user
         managers = Group.objects.get(name="managers")
         managers.delete()
         u = User.objects.get(username='user')
-        u.delete()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       
+        u.delete()       
